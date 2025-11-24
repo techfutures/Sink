2fc105d6e6dcb82048a9d0901ce65b938eaf2ec4
Andrew Gee <agee@techfutures.xyz>
2025-11-24 15:25:32 -0800

    Change app title and description
    Updated the title and description in app configuration.

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,12 +1,12 @@
 export default defineAppConfig({
-  title: 'Tech Futures Link Shortener',
+  title: 'TF l/s',
   email: 'info@techfutures.xyz',
   github: 'https://github.com/ccbikai/sink',
   twitter: 'https://sink.cool/kai',
   telegram: 'https://sink.cool/telegram',
   mastodon: 'https://sink.cool/mastodon',
   blog: 'https://sink.cool/blog',
-  description: 'Secure Link Shortener',
+  description: 'Tech Futures Secure Link Shortener',
   image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
